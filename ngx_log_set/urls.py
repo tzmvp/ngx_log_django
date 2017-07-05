999ca065df8cd4f4fded2f501e342a100476f3b7
tzmvp <t_zhen@139.com>
2017-07-05 10:30:48 +0800

    Create urls.py

---
+++ ngx_log_set/urls.py
@@ -0,0 +1,27 @@
+"""ngx_log URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.8/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Add an import:  from blog import urls as blog_urls
+    2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
+"""
+from django.conf.urls import include, url
+from django.contrib import admin
+from ngx_log_view.views import get_info,get_pv_count,Index,Test,Test_table
+
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^domain$', get_info),
+    url(r'^get$', get_pv_count),
+    url(r'^index$', Index),
+    url(r'^test$', Test),
+    url(r'^tb$', Test_table),
+]
